3a91b8955ed5dde70762bec305b0e40a222a52e5
Chang-Man <changagim@snu.ac.kr>
2022-09-09 23:12:23 +0900

    install emotion/styled

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled';
 import Head from 'next/head';
 import { ReactElement } from 'react';
 
@@ -6,14 +7,20 @@ interface LayoutProps {
   children: ReactElement;
   seoTitle?: string;
 }
+
+const Header = styled.div({
+  backgroundColor: 'black',
+  height: 50,
+});
+
 export default function Layout({ title, seoTitle, children }: LayoutProps) {
   return (
     <div>
       <Head>
         <title>{seoTitle ? `${title} | BillionSugar` : 'BillionSugar'}</title>
       </Head>
-      <div className="bg-black shadow-lg w-full h-14 justify-center text-lg px-10 font-medium fixed text-gray-800 border-b top-0 flex items-center"></div>
-      <div className="h-screen pt-14">{children}</div>
+      <Header />
+      <div>{children}</div>
     </div>
   );
 }
