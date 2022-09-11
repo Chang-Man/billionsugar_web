e04dad109556f7b80766f48eaaf5ddef99e67413
Chang-Man <changagim@snu.ac.kr>
2022-09-11 12:48:07 +0900

    main Book Item

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,7 @@ import styled from '@emotion/styled';
 import type { NextPage } from 'next';
 import Layout from '../components/layout';
 import facepaint from 'facepaint';
-
-const MainItem = styled.div({
-  backgroundColor: 'pink',
-  width: '100%',
-  height: 200,
-});
+import BookItem from '../components/bookItem';
 
 const MainItems = styled.div((props) =>
   mq({
@@ -21,14 +16,19 @@ const breakpoints = [576, 768, 992, 1200];
 
 const mq = facepaint(breakpoints.map((bp) => `@media (min-width: ${bp}px)`));
 
+const dummy = [
+  { title: 'string', id: 1, image: 'string', price: 5000, author: 'author0' },
+  { title: 'string', id: 2, image: 'string', price: 25000, author: 'author1' },
+  { title: 'string', id: 3, image: 'string', price: 15000, author: 'author2' },
+];
+
 const Home: NextPage = () => {
   return (
     <Layout title="Home">
       <MainItems>
-        <MainItem />
-        <MainItem />
-        <MainItem />
-        <MainItem />
+        {dummy.map((item) => (
+          <BookItem key={item.id} id={item.id} title={item.title} image={item.image} price={item.price} author={item.author} />
+        ))}
       </MainItems>
     </Layout>
   );
